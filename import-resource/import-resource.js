e7be36bbebc81b439aa70161ff4561440e017e0e
travis-ci-d <travis-ci-d@repo.ren>
2017-12-10 01:19:10 +0800

    some modification.

--- import-resource/import-resource.js
+++ import-resource/import-resource.js
@@ -3,7 +3,7 @@ movingDiv.style.left = 0;
 var deviceWidth = document.body.clientWidth || document.documentElement.clientWidth;
 // var maxLeft = deviceWidth - movingDiv.offsetWidth;
 setInterval(function(){
-  movingDiv.style.left = parseFloat(movingDiv.style.left) + 5 + 'px';
+  movingDiv.style.left = parseFloat(movingDiv.style.left) + 1 + 'px';
   console.log(movingDiv.style.left);
   if(parseFloat(movingDiv.style.left) + 5 >= deviceWidth) {
     movingDiv.style.left = -movingDiv.offsetWidth;
