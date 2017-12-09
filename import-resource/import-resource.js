cd2acac1ea0c919b5d2933a1021c24b755ca1d4b
travis-ci-d <travis-ci-d@repo.ren>
2017-12-10 01:08:28 +0800

    some modification.

--- import-resource/import-resource.js
+++ import-resource/import-resource.js
@@ -1,4 +1,6 @@
 const movingDiv = document.getElementById('moving');
+movingDiv.style.left = 0;
 setInterval(function(){
   movingDiv.style.left = parseFloat(movingDiv.style.left) + 5 + 'px';
+  console.log(movingDiv.style.left);
 }, 17);
